d43662eb9d7b33e08948617e0e8baa85ebdaf533
Zbyszek <zbyszek.bulecki@gmail.com>
2022-01-04 16:33:53 +0100

    adding fileScanner for tools list

--- src/Simulation.java
+++ src/Simulation.java
@@ -5,8 +5,16 @@ import java.util.Scanner;
 
 public class Simulation {
     File toolsList = new File("tools.txt");
-        Scanner fileScanner = new Scanner(toolsList);
+
+    Scanner fileScanner = new Scanner(toolsList);
+
+    ArrayList phase1Tools = new ArrayList(); // Array containing the tools file.
+
+    while (fileScanner.hasNextLine()) { // Loop for reading the tools file.
+        phase1Tools.add(fileScanner.nextLine());
+    }
 
 
-
+    public Simulation() throws FileNotFoundException {
+    }
 }
